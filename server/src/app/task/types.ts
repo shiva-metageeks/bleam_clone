5cdf13510fd13279278c95c55ee46ac0d3dff772
shiva-metageeks <shiva@metageeks.tech>
2024-10-11 16:40:14 +0530

    task query mutation

---
+++ server/src/app/task/types.ts
@@ -0,0 +1,13 @@
+export const types = `#graphql
+
+     input CreateTaskInput {
+        name: String!
+        taskId: String!
+        description: String!
+        points: Int!
+        media: String
+        type: String!
+        
+    }
+
+`
