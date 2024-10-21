533cfcd869b2c13b0f3033c7e166ff15f2e2ece9
shiva-metageeks <shiva@metageeks.tech>
2024-10-21 10:59:59 +0530

    aws getUrl issue resolved

--- client/src/utils/helper/aws/s3.ts
+++ client/src/utils/helper/aws/s3.ts
@@ -1,8 +1,28 @@
-import { getPresignedUrlQuery } from "@/graphql/query/aws";
+import { gql } from "graphql-request";
 import { graphqlClient } from "@/clients/api"
 
+const getPresignedUrlQuery = gql`
+query getPresignedUrl($fileName: String!, $contentType: String!) {
+  getPresignedUrl(fileName: $fileName, contentType: $contentType)
+}
+`
+interface PresignedUrlResult {
+   getPresignedUrl: string;
+ }
+ 
+ interface PresignedUrlVariables {
+   fileName: string;
+   contentType: string;
+ }
+
+
 export const getUploadUrl = async (fileName: string, contentType: string): Promise<string | null> => {
-   const { getPresignedUrl:uploadUrl } = await graphqlClient.request(getPresignedUrlQuery, { fileName, contentType })
-   console.log("response",uploadUrl);
-   return uploadUrl as string | null;
+  
+   try {
+    const { getPresignedUrl } = await graphqlClient.request<PresignedUrlResult, PresignedUrlVariables>(getPresignedUrlQuery, { fileName, contentType })
+    return getPresignedUrl;
+   } catch (error) {
+    console.error("Error fetching presigned URL:", error);
+    return null;
+   }
 }
